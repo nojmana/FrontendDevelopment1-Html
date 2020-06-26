31a8fe1d960fa6de90b2308aca7e695114ccc41d
nojmana <neumannkinga7@gmail.com>
2020-06-26 03:27:56 +0200

    JS2-Slideshow

--- scripts.js
+++ scripts.js
@@ -1,4 +1,27 @@
-window.setInterval(function(){
+// display date
+window.setInterval(function () {
     var date = new Date();
     document.getElementById("js-date").innerHTML = date.toDateString()
 }, 1000);
+
+
+// slideshow
+var slideIndex = 0;
+slideshow();
+
+function slideshow() {
+    var pics = document.getElementsByClassName("kids-pic-js");
+    for (i = 0; i < pics.length; i++) {
+        pics[i].style.display = "none";
+    }
+
+    slideIndex++;
+
+    if (slideIndex > pics.length) { 
+        slideIndex = 1 
+    }
+    pics[slideIndex - 1].style.display = "block";
+    
+    var timeout = 3000;
+    setTimeout(slideshow, timeout);
+}
